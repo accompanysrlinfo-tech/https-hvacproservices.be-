1bd41db66f2fa2f08ea793de39a5c7ca99c994c9
accompanysrlinfo-tech <accompany.srlinfo@gmail.com>
2026-04-30 14:19:33 +0200

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,15 +4,15 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
-  const body = await req.json();
-
   try {
+    const body = await req.json();
+
     await resend.emails.send({
       from: "A&C Company <onboarding@resend.dev>",
       to: "Accompany.srlinfo@gmail.com",
       subject: "Noua cerere de pe site",
       html: `
-        <h2>Nou mesaj</h2>
+        <h2>Nou message</h2>
         <p><strong>Nom:</strong> ${body.name}</p>
         <p><strong>Email:</strong> ${body.email}</p>
         <p><strong>Message:</strong> ${body.message}</p>
@@ -20,7 +20,9 @@ export async function POST(req: Request) {
     });
 
     return NextResponse.json({ success: true });
+
   } catch (error) {
-    return NextResponse.json({ error });
+    console.error(error);
+    return NextResponse.json({ error: "Erreur envoi email" }, { status: 500 });
   }
 }
